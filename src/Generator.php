650830a217440aa501ef4bf912350bff149661fb
Alexey Starikov <alexxisgreat@google.com>
2021-05-14 14:10:02 +0000

    fix lint errors in Generator.php

--- src/Generator.php
+++ src/Generator.php
@@ -4,21 +4,20 @@ namespace Project\Lvl1\S482\Generator;
 
 function isAnswerCorrect($game, $number, $answer)
 {
-    switch($game)
-    {
+    switch ($game) {
         case "even":
             if ($number % 2 === 0) {
                 $correctAnswer = 'yes';
             } else {
                 $correctAnswer = 'no';
             }
-        
+
             if ($answer === $correctAnswer) {
                 return true;
             } else {
                 return false;
             }
-        
+
             break;
         default:
             echo "Uknown game for the Generator";
