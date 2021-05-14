d841817b0c14fb0f6c990a9ec0ab972d4c79e4b6
Alexey Starikov <alexxisgreat@google.com>
2021-05-14 15:21:05 +0000

    move the Calc to the Engine

--- games/Calc.php
+++ games/Calc.php
@@ -2,7 +2,7 @@
 
 namespace BrainGames\Calc;
 
-use function cli\line;
+use function Project\Lvl1\S482\Engine\play;
 
 function randomAction()
 {
@@ -34,42 +34,5 @@ function correctAnswer($act, $firstNum, $secondNum)
 
 function run()
 {
-    $attempt = 1;
-
-    line("Welcome to the Brain Games!");
-    line("What is the result of the expression?\n");
-    $name = \cli\prompt('May I have your name?');
-    line("Hello, %s!", $name, "\n");
-    //line("\n");
-
-    while ($attempt < 4) {
-        $firstNumber = random_int(-100, 100);
-        $secondNumber = random_int(-100, 100);
-        $action = randomAction();
-        $firstNumberAsString = (string) $firstNumber;
-        $secondNumberAsString = (string) $secondNumber;
-        $expressionAsString = "{$firstNumberAsString} {$action} {$secondNumberAsString}";
-
-        line("Question: %s", $expressionAsString);
-        $answer = \cli\prompt('Your answer is');
-        $isCheckGood = isAnswerCorrect($action, $firstNumber, $secondNumber, $answer);
-        $corrAnswer = correctAnswer($action, $firstNumber, $secondNumber);
-
-        if ($isCheckGood === true) {
-            line("Question: {$expressionAsString}");
-            line("Your answer: {$answer}");
-            line("Correct!");
-            $attempt += 1;
-        } else {
-            line("Question: {$expressionAsString}");
-            line("Your answer: {$answer}");
-            line("'{$answer}' is the wrong answer ;(. The correct answer was '{$corrAnswer}'.");
-            line("Let's try again, %s!", $name);
-            break;
-        }
-    }
-
-    if ($attempt > 3) {
-        line("Congratulations, %s!", $name);
-    }
+    play('calc');
 }
--- src/Engine.php
+++ src/Engine.php
@@ -39,7 +39,44 @@ function play($game)
 
             break;
         case "calc":
-            echo "Good choice!";
+            $attempt = 1;
+
+            line("Welcome to the Brain Games!");
+            line("What is the result of the expression?\n");
+            $name = \cli\prompt('May I have your name?');
+            line("Hello, %s!", $name, "\n");
+
+            while ($attempt < 4) {
+                $firstNumber = random_int(-100, 100);
+                $secondNumber = random_int(-100, 100);
+                $action = randomAction();
+                $firstNumberAsString = (string) $firstNumber;
+                $secondNumberAsString = (string) $secondNumber;
+                $expressionAsString = "{$firstNumberAsString} {$action} {$secondNumberAsString}";
+
+                line("Question: %s", $expressionAsString);
+                $answer = \cli\prompt('Your answer is');
+                $isCheckGood = isAnswerCorrect($action, $firstNumber, $secondNumber, $answer);
+                $corrAnswer = correctAnswer($action, $firstNumber, $secondNumber);
+
+                if ($isCheckGood === true) {
+                    line("Question: {$expressionAsString}");
+                    line("Your answer: {$answer}");
+                    line("Correct!");
+                    $attempt += 1;
+                } else {
+                    line("Question: {$expressionAsString}");
+                    line("Your answer: {$answer}");
+                    line("'{$answer}' is the wrong answer ;(. The correct answer was '{$corrAnswer}'.");
+                    line("Let's try again, %s!", $name);
+                    break;
+                }
+            }
+
+            if ($attempt > 3) {
+                line("Congratulations, %s!", $name);
+            }
+
             break;
         case "gcd":
             echo "Bad choice!";
