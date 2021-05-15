7480bf5cc6f8980d7311bccadf250af066ef52d7
Alexey Starikov <alexxisgreat@google.com>
2021-05-15 13:47:35 +0000

    move the Prime run function to the Engine.php

--- games/Prime.php
+++ games/Prime.php
@@ -2,12 +2,7 @@
 
 namespace BrainGames\Prime;
 
-use function cli\line;
-
-function randomNumber()
-{
-    return random_int(1, 500);
-}
+use function Project\Lvl1\S482\Engine\play;
 
 function isAnswerCorrect($num, $answ)
 {
@@ -31,37 +26,5 @@ function correctAnswer($num)
 
 function run()
 {
-    $attempt = 1;
-
-    line("Welcome to the Brain Games!");
-    line("Answer \"yes\" if the given number is prime. Otherwise answer \"no\".\n");
-    $name = \cli\prompt('May I have your name?');
-    line("Hello, %s!", $name, "\n");
-
-    while ($attempt < 4) {
-        $firstNumber = randomNumber();
-        $expressionAsString = "{$firstNumber}";
-
-        line("Question: %s", $expressionAsString);
-        $answer = \cli\prompt('Your answer is');
-        $isCheckGood = isAnswerCorrect($firstNumber, $answer);
-        $corrAnswer = correctAnswer($firstNumber);
-
-        if ($isCheckGood === true) {
-            line("Question: {$expressionAsString}");
-            line("Your answer: {$answer}");
-            line("Correct!");
-            $attempt += 1;
-        } else {
-            line("Question: {$expressionAsString}");
-            line("Your answer: {$answer}");
-            line("'{$answer}' is the wrong answer ;(. The correct answer was '{$corrAnswer}'.");
-            line("Let's try again, %s!", $name);
-            break;
-        }
-    }
-
-    if ($attempt > 3) {
-        line("Congratulations, %s!", $name);
-    }
+    play('prime');
 }
--- src/Engine.php
+++ src/Engine.php
@@ -169,7 +169,40 @@ function play($game)
 
             break;
         case "prime":
-            echo "prime";
+            $attempt = 1;
+
+            line("Welcome to the Brain Games!");
+            line("Answer \"yes\" if the given number is prime. Otherwise answer \"no\".\n");
+            $name = \cli\prompt('May I have your name?');
+            line("Hello, %s!", $name, "\n");
+
+            while ($attempt < 4) {
+                $firstNumber = random_int(1, 500);
+                $expressionAsString = "{$firstNumber}";
+
+                line("Question: %s", $expressionAsString);
+                $answer = \cli\prompt('Your answer is');
+                $isCheckGood = isAnswerCorrect($firstNumber, $answer);
+                $corrAnswer = correctAnswer($firstNumber);
+
+                if ($isCheckGood === true) {
+                    line("Question: {$expressionAsString}");
+                    line("Your answer: {$answer}");
+                    line("Correct!");
+                    $attempt += 1;
+                } else {
+                    line("Question: {$expressionAsString}");
+                    line("Your answer: {$answer}");
+                    line("'{$answer}' is the wrong answer ;(. The correct answer was '{$corrAnswer}'.");
+                    line("Let's try again, %s!", $name);
+                    break;
+                }
+            }
+
+            if ($attempt > 3) {
+                line("Congratulations, %s!", $name);
+            }
+
             break;
         default:
             echo "Uknown game!";
