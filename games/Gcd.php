60d44c123763923cdd4c65a3a37d2627879bfffc
Alexey Starikov <alexxisgreat@google.com>
2021-05-15 09:52:32 +0000

    move the Gcs run function to the Engine.php

--- games/Gcd.php
+++ games/Gcd.php
@@ -2,7 +2,7 @@
 
 namespace BrainGames\Gcd;
 
-use function cli\line;
+use function Project\Lvl1\S482\Engine\play;
 
 function randomNumber()
 {
@@ -52,40 +52,5 @@ function correctAnswer($firstNum, $secondNum)
 
 function run()
 {
-    $attempt = 1;
-
-    line("Welcome to the Brain Games!");
-    line("Find the greatest common divisor of given numbers\n");
-    $name = \cli\prompt('May I have your name?');
-    line("Hello, %s!", $name, "\n");
-
-    while ($attempt < 4) {
-        $firstNumber = randomNumber();
-        $secondNumber = randomNumber();
-        $firstNumberAsString = (string) $firstNumber;
-        $secondNumberAsString = (string) $secondNumber;
-        $expressionAsString = "{$firstNumberAsString} {$secondNumberAsString}";
-
-        line("Question: %s", $expressionAsString);
-        $answer = \cli\prompt('Your answer is');
-        $isCheckGood = isAnswerCorrect($firstNumber, $secondNumber, $answer);
-        $corrAnswer = correctAnswer($firstNumber, $secondNumber);
-
-        if ($isCheckGood === true) {
-            line("Question: {$expressionAsString}");
-            line("Your answer: {$answer}");
-            line("Correct!");
-            $attempt += 1;
-        } else {
-            line("Question: {$expressionAsString}");
-            line("Your answer: {$answer}");
-            line("'{$answer}' is the wrong answer ;(. The correct answer was '{$corrAnswer}'.");
-            line("Let's try again, %s!", $name);
-            break;
-        }
-    }
-
-    if ($attempt > 3) {
-        line("Congratulations, %s!", $name);
-    }
+    play('gcd');
 }
--- src/Engine.php
+++ src/Engine.php
@@ -83,7 +83,43 @@ function play($game)
 
             break;
         case "gcd":
-            echo "Bad choice!";
+            $attempt = 1;
+
+            line("Welcome to the Brain Games!");
+            line("Find the greatest common divisor of given numbers\n");
+            $name = \cli\prompt('May I have your name?');
+            line("Hello, %s!", $name, "\n");
+
+            while ($attempt < 4) {
+                $firstNumber = randomNumber();
+                $secondNumber = randomNumber();
+                $firstNumberAsString = (string) $firstNumber;
+                $secondNumberAsString = (string) $secondNumber;
+                $expressionAsString = "{$firstNumberAsString} {$secondNumberAsString}";
+
+                line("Question: %s", $expressionAsString);
+                $answer = \cli\prompt('Your answer is');
+                $isCheckGood = isAnswerCorrect($firstNumber, $secondNumber, $answer);
+                $corrAnswer = correctAnswer($firstNumber, $secondNumber);
+
+                if ($isCheckGood === true) {
+                    line("Question: {$expressionAsString}");
+                    line("Your answer: {$answer}");
+                    line("Correct!");
+                    $attempt += 1;
+                } else {
+                    line("Question: {$expressionAsString}");
+                    line("Your answer: {$answer}");
+                    line("'{$answer}' is the wrong answer ;(. The correct answer was '{$corrAnswer}'.");
+                    line("Let's try again, %s!", $name);
+                    break;
+                }
+            }
+
+            if ($attempt > 3) {
+                line("Congratulations, %s!", $name);
+            }
+
             break;
         case "progression":
             echo "progr";
